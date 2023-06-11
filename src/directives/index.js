c1ab114a78c7c00c4282c01259ceba93126c750c
unknown <1390919155@qq.com>
2023-06-11 11:43:05 +0800

    图片懒加载优化

---
+++ src/directives/index.js
@@ -0,0 +1,26 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install(app) {
+        // 定义全局指令
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                // el:指令绑定的元素 img
+                // binding: 最常用绑定value, 表达式的值, 图片url
+                console.log(el, binding.value)
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        console.log(isIntersecting)
+                        if (isIntersecting) {
+                            // 进入视口区域
+                            el.src = binding.value
+                            stop()
+                        }
+                    },
+                )
+            }
+        })
+    }
+}
