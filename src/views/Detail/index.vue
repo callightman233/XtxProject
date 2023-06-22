7e1d0745727d477c8720d1e7a38a6696e0b3435e
unknown <1390919155@qq.com>
2023-06-22 15:24:13 +0800

    sku组件熟悉

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 import { getDetail } from '@/apis/detail'
 import { useRoute } from 'vue-router'
 import { ref, onMounted } from 'vue'
@@ -12,6 +13,10 @@ const getGoods = async () => {
 }
 
 onMounted(() => getGoods())
+
+const skuChange = (sku) => {
+    console.log(sku)
+}
 </script>
 
 <template>
@@ -40,7 +45,7 @@ onMounted(() => getGoods())
                     <div class="goods-info">
                         <div class="media">
                             <!-- 图片预览区 -->
-                            <ImageView :image-list="goods.mainPictures"/>
+                            <ImageView :image-list="goods.mainPictures" />
                             <!-- 统计数量 -->
                             <ul class="goods-sales">
                                 <li>
@@ -89,7 +94,7 @@ onMounted(() => getGoods())
                                 </dl>
                             </div>
                             <!-- sku组件 -->
-
+                            <XtxSku :goods="goods" @change="skuChange" />
                             <!-- 数据组件 -->
 
                             <!-- 按钮组件 -->
@@ -125,9 +130,9 @@ onMounted(() => getGoods())
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
                             <!-- 24小时 -->
-                            <DetailHot :hot-type="1"/>
+                            <DetailHot :hot-type="1" />
                             <!-- 周榜 -->
-                            <DetailHot :hot-type="2"/>
+                            <DetailHot :hot-type="2" />
                         </div>
                     </div>
                 </div>
