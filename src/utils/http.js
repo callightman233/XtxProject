4f03b7c4feb3e898fb4873bf28accedcedfa3976
unknown <1390919155@qq.com>
2023-06-23 14:25:14 +0800

    登录-请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios"
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/user'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -10,6 +11,11 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
@@ -17,8 +23,8 @@ httpInstance.interceptors.request.use(config => {
 httpInstance.interceptors.response.use(res => res.data, e => {
     // 统一错误提示
     ElMessage({
-        type:'warning',
-        message:e.response.data.msg
+        type: 'warning',
+        message: e.response.data.msg
     })
     return Promise.reject(e)
 })
